64d167bf7f4e31bfecb80635c836bfc75bb7d38a
Bar Cohen <94045477+BarcDevs@users.noreply.github.com>
2025-08-11 21:00:22 +0300

    feat(hero): add features section

---
+++ src/constants/hero/features.ts
@@ -0,0 +1,8 @@
+import { Locales } from '@/lib/language/keys'
+
+export const FEATURES = [
+    Locales.hero_feature_fast,
+    Locales.hero_feature_performance,
+    Locales.hero_feature_seo,
+    Locales.hero_feature_responsive
+]
