32075ae1f7a4ee83eee77db9aeb1ddd6c7d421f5
Bar Cohen <94045477+BarcDevs@users.noreply.github.com>
2025-08-11 18:10:03 +0300

    rfc(language): modify language picker to suit next-intl

--- src/components/shared/navbar/language-picker.tsx
+++ src/components/shared/navbar/language-picker.tsx
@@ -1,17 +1,25 @@
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
-import { useLanguage } from '@/context/language'
+'use client'
 
-const LanguagePicker = ({}) => {
-    const {
-        lang,
-        setLang,
-        dir
-    } = useLanguage()
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
+import { useLocale } from 'next-intl'
+import { useRouter } from 'next/navigation'
+
+const LanguagePicker = () => {
+    const currentLocale = useLocale()
+    const router = useRouter()
+
+    const onLanguagePick = (newLocale: 'en' | 'he') => {
+        if ( newLocale === currentLocale ) return
+
+        document.cookie = `NEXT_LOCALE=${newLocale}; path=/; max-age=31536000`
+
+        router.refresh()
+    }
 
     return (
         <Select
-            value={lang}
-            onValueChange={(v: 'en' | 'he') => setLang(v)}
+            value={currentLocale}
+            onValueChange={(value: 'en' | 'he') => onLanguagePick(value)}
         >
             <SelectTrigger
                 className="w-[110px]"
@@ -20,7 +28,7 @@ const LanguagePicker = ({}) => {
                 <SelectValue placeholder="Language"/>
             </SelectTrigger>
 
-            <SelectContent align={dir === 'rtl' ? 'start' : 'end'}>
+            <SelectContent align={currentLocale === 'he' ? 'start' : 'end'}>
                 <SelectItem value="en">English</SelectItem>
                 <SelectItem value="he">עברית</SelectItem>
             </SelectContent>
