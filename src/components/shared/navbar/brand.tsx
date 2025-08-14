f62b3ff395ae24fdbb136591ba9142087c608e5b
Bar Cohen <94045477+BarcDevs@users.noreply.github.com>
2025-08-14 16:28:28 +0300

    fix: brand still being sent to use translation although it is no longer as locale

--- src/components/shared/navbar/brand.tsx
+++ src/components/shared/navbar/brand.tsx
@@ -10,7 +10,7 @@ const Brand = ({}) => {
         <Link
             href={NAV_LINKS.home.href}
             className="flex items-center gap-2"
-            aria-label={t(NAV_LINKS.home.locale)}
+            aria-label={NAV_LINKS.home.locale}
         >
             <div
                 className="h-8 w-8 flex items-center justify-center rounded-md bg-[#77c6b0] text-[#3e2f34]">
@@ -24,7 +24,7 @@ const Brand = ({}) => {
             </div>
             <span className="font-bold tracking-tight text-[#3e2f34]">
                 {/*TODO: change font*/}
-                {t(NAV_LINKS.home.locale)}
+                {NAV_LINKS.home.locale}
             </span>
         </Link>
     )
