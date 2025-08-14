22155e52a62a6273e1c361c4143233c09c8f5406
Bar Cohen <94045477+BarcDevs@users.noreply.github.com>
2025-08-14 16:25:23 +0300

    feat(portfolio): add project image comp

---
+++ src/components/homepage/portfolioSection/project-image.tsx
@@ -0,0 +1,42 @@
+import Image from 'next/image'
+import { FC } from 'react'
+import { Project } from '@/types/project'
+
+type ProjectImageProps = {
+    project: Project;
+    variant?: 'preview' | 'detail';
+    scale?: number;
+}
+
+const ProjectImage: FC<ProjectImageProps> = ({
+    project,
+    variant = 'preview',
+    scale
+}) => {
+    const { w, h } = project.image
+
+    const factor = scale ?? ( variant === 'detail' ? 1.2 : 1 )
+    const width = w && Math.round(w * factor)
+    const height = h && Math.round(h * factor)
+
+    const variantClasses = {
+        preview: 'aspect-[16/11]',
+        detail: 'rounded-md border'
+    }
+
+    return (
+        <Image
+            src={project.image.src ??
+                `/placeholder.svg?height=${height}&width=${width}&query=${
+                    encodeURIComponent(
+                        project.image.query
+                    )}`}
+            width={width}
+            height={height}
+            alt={`${project.title} ${variant}`}
+            className={`w-full object-cover ${variantClasses[ variant ] || ''}`}
+        />
+    )
+}
+
+export default ProjectImage
