f107dd2c1e4c292a90d947c4a8e0fe12e4573769
Bar Cohen <94045477+BarcDevs@users.noreply.github.com>
2025-08-14 16:25:16 +0300

    feat(portfolio): add project link comp

---
+++ src/components/homepage/portfolioSection/project-link.tsx
@@ -0,0 +1,33 @@
+import { ExternalLink } from 'lucide-react'
+import { useTranslations } from 'next-intl'
+import Link from 'next/link'
+import { FC } from 'react'
+
+import { Locales } from '@/lib/language/keys'
+import { Button } from '@/components/ui/button'
+
+type ProjectLinkProps = {
+    link: string;
+}
+
+const ProjectLink: FC<ProjectLinkProps> = ({ link }) => {
+    const t = useTranslations()
+
+    return (
+        <div>
+            <Link
+                href={link}
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <Button variant="outline"
+                        className="gap-2 border-[#77c6b0] text-[#3e2f34]">
+                    <ExternalLink className="h-4 w-4"/>
+                    {t(Locales.portfolio_preview)}
+                </Button>
+            </Link>
+        </div>
+    )
+}
+
+export default ProjectLink
