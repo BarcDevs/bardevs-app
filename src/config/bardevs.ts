237864ffcbe7610efb3451a00940d52204adb970
Bar Cohen <94045477+BarcDevs@users.noreply.github.com>
2025-08-20 13:24:58 +0300

    rfc: add mail to config

--- src/config/bardevs.ts
+++ src/config/bardevs.ts
@@ -3,10 +3,11 @@ export const appConfig = {
     socials: {
         github: 'https://github.com/BarcDevs/',
         linkedin: 'https://www.linkedin.com/in/barcohendev/',
-        facebook: 'https://www.facebook.com/profile.php?id=61579354492228/'
+        facebook: 'https://www.facebook.com/profile.php?id=61579354492228%2F/'
     },
     whatsapp: {
         number: '972552644558',
         message: 'Hello Bar, I’m interested in a professional website. Can you share more details?'
-    }
+    },
+    email: 'barcprodevelopments@gmail.com'
 }
