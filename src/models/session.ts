c4223b246356389caa4ebfd7fe0acd2b8c46f2da
Bar Cohen <94045477+BarcDevs@users.noreply.github.com>
2025-08-27 12:19:56 +0300

    rfc(middleware): add type for session model

---
+++ src/models/session.ts
@@ -0,0 +1,39 @@
+import {
+    Schema,
+    model,
+    models,
+    InferSchemaType
+} from 'mongoose'
+
+const sessionSchema = new Schema(
+    {
+        sessionId: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        browser: { type: String },
+        os: { type: String },
+        device: {
+            type: String,
+            default: 'desktop'
+        },
+        language: { type: String },
+        referrer: { type: String },
+        ip: { type: String }
+    },
+    {
+        timestamps: {
+            createdAt: 'createdAt',
+            updatedAt: false
+        },
+        collection: 'Session',
+        versionKey: false
+    }
+)
+
+export const Session =
+    models.Session ||
+    model('Session', sessionSchema)
+
+export type SessionType = InferSchemaType<typeof sessionSchema>
