078acdeef3390e55d35d479506bb608dc91e539f
Bar Cohen <94045477+BarcDevs@users.noreply.github.com>
2025-08-27 17:19:06 +0300

    rfc(tracking): add credentials sending to axios instance

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,12 +4,14 @@ export const apiClient = axios.create({
     baseURL: '/api',
     headers: {
         'Content-Type': 'application/json'
-    }
+    },
+    withCredentials: true
 })
 
 export const apiServer = axios.create({
     baseURL: process.env.NEXT_PUBLIC_BASE_URL + '/api',
     headers: {
         'Content-Type': 'application/json'
-    }
+    },
+    withCredentials: true
 })
