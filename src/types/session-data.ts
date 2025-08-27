8acc92efb5a04450728dcc2d38cf4e34eca7f450
Bar Cohen <94045477+BarcDevs@users.noreply.github.com>
2025-08-27 15:46:22 +0300

    rfc: add session type

---
+++ src/types/session-data.ts
@@ -0,0 +1,10 @@
+export type SessionData = {
+    sessionId: string
+    ip?: string
+    browser?: string
+    os?: string
+    device: string
+    userAgent?: string
+    referrer?: string | null
+    language?: string | null
+}
