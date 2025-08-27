ed6259917969b535a4a8956707ea62e39cc7c508
Bar Cohen <94045477+BarcDevs@users.noreply.github.com>
2025-08-27 19:25:03 +0300

    rfc(tracking): wrap trackSession with useEffect to enforce client accessing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 'use client'
 
 import { useSearchParams } from 'next/navigation'
+import { useEffect } from 'react'
 import { trackSession } from '@/actions/track-session'
 import HomePage from '@/components/pages/home'
 
-
 const Page = ({}) => {
     const searchParams = useSearchParams()
-    trackSession(searchParams.get('source') || 'unknown')
+    useEffect(() => {
+        trackSession(searchParams.get('source') || 'unknown')
+    }, [])
 
     return (
         <HomePage/>
